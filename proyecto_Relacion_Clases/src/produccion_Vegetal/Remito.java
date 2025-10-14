6a1d01f7b7fb86eebd6957c01041ecb0a3d59822
Javier <javierlocane@gmail.com>
2025-10-14 17:17:45 -0300

    Se crea una relacion de dependencia y se agrega la clase remito

---
+++ proyecto_Relacion_Clases/src/produccion_Vegetal/Remito.java
@@ -0,0 +1,19 @@
+package produccion_Vegetal;
+
+public class Remito {
+	
+	private String descripcionPedido;
+
+	public Remito(String descripcionPedido) {
+		this.descripcionPedido = descripcionPedido;
+	}
+
+	public String getDescripcionPedido() {
+		return descripcionPedido;
+	}
+
+	public void setDescripcionPedido(String descripcionPedido) {
+		this.descripcionPedido = descripcionPedido;
+	}
+	
+}
