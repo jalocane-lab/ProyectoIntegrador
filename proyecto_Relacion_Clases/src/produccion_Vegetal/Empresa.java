49ca4f6bd194aa1471bcb485deb01759c39cd42f
Javier <javierlocane@gmail.com>
2025-10-16 14:51:45 -0300

    Se agregan atributos y metodo infoEmpresa a la clase "Empresa"

--- proyecto_Relacion_Clases/src/produccion_Vegetal/Empresa.java
+++ proyecto_Relacion_Clases/src/produccion_Vegetal/Empresa.java
@@ -3,12 +3,21 @@ package produccion_Vegetal;
 public class Empresa {
 	
 	private Area areas;
-	public Empresa(Area areas) {
+	private String nombre;
+	private int cuit;
+	
+	public Empresa(Area areas, String nombre, int cuit) {
 		this.areas = areas;
+		this.nombre = nombre;
+		this.cuit = cuit;
+	}
+	
+	public void infoEmpresa() {
+		System.out.println("* Nombre de la Empresa: " + nombre + " --> CUIT: " + cuit);
 	}
 	
 	public void mostrarAreas() {
-		System.out.println(areas.getArea1() + "  " + areas.getArea2() + areas.getArea3());
+		System.out.println("--> Áreas de la Empresa: " + areas.getArea1() + "  " + areas.getArea2() + areas.getArea3());
 	}
 
 }
