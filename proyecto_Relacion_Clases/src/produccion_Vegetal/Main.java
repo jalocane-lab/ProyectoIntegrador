8e5db99568352de4dac21c00a305c6d23a7f8a0d
Javier <javierlocane@gmail.com>
2025-10-16 15:28:42 -0300

    Correccion de argumentos

--- proyecto_Relacion_Clases/src/produccion_Vegetal/Main.java
+++ proyecto_Relacion_Clases/src/produccion_Vegetal/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Area areasEmpresa = new Area ("Produccion", "RRHH ", " Ventas");
-		Empresa empresa = new Empresa (areasEmpresa, "Dulces puelo", 2065362566);
+		Empresa empresa = new Empresa (areasEmpresa, "Dulces Puelo", 2065362566);
 
 		empresa.infoEmpresa();
 		empresa.mostrarAreas();
